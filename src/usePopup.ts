afdf2234819dd191da37e9d4ff3a4f461ed1a74e
Aidan White <aidan.white36@gmail.com>
2020-12-08 23:20:50 -0600

    fix: return type on usePopup hook

--- src/usePopup.ts
+++ src/usePopup.ts
@@ -1,7 +1,12 @@
 import { useContext, useEffect } from 'react';
 import { PopupContext } from './PopupContext';
 
-export function usePopup(key: string, popupRenderer: PopupRenderer) {
+type ReturnType = [
+    (message: string) => void,
+    () => void,
+]
+
+export function usePopup(key: string, popupRenderer: PopupRenderer): ReturnType {
     const { addPopup, removePopup, displayPopup, closePopup } = useContext(PopupContext);
     useEffect(() => {
         addPopup(key, popupRenderer);
